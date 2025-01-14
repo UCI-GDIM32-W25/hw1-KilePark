a55d89edd7c61a6140043c30972057b2c9590a41
KilePark <kcpark1@uci.edu>
2025-01-13 16:50:12 -0800

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,6 +9,11 @@ public class Player : MonoBehaviour
     [SerializeField] private GameObject _plantPrefab;
     [SerializeField] private int _numSeeds = 5; 
     [SerializeField] private PlantCountUI _plantCountUI;
+
+    public Sprite FaceUp;
+    public Sprite FaceDown;
+    public Sprite FaceLeft;
+    public Sprite FaceRight;
 
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
@@ -18,7 +25,33 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<SpriteRenderer>().sprite = FaceUp;
+            pos.y += _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<SpriteRenderer>().sprite = FaceLeft;
+            pos.x -= _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<SpriteRenderer>().sprite = FaceDown;
+            pos.y -= _speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<SpriteRenderer>().sprite = FaceRight;
+            pos.x += _speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
     }
 
     public void PlantSeed ()
